2c4b566a40d606007328eda586deae00047a79e7
hangman-css <pcdanielpxtt@outlook.com>
2023-03-16 19:57:56 -0300

    tabela
    tabela

---
+++ js.js
@@ -0,0 +1,5 @@
+// Pergunta ao usuário o nome dele
+let nome = window.prompt("Qual é o seu nome?");
+
+// Mostra uma mensagem de boas-vindas na tela
+window.alert("Olá " + nome + "! Seja bem-vindo(a)!");
