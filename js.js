32d568b34c8bf86398d8af0c490d48b0d5196be9
hangman-css <pcdanielpxtt@outlook.com>
2023-03-16 22:45:51 -0300

    final
    final

--- js.js
+++ js.js
@@ -1,5 +1,2 @@
-// Pergunta ao usuário o nome dele
-let nome = window.prompt("Qual é o seu nome?");
 
-// Mostra uma mensagem de boas-vindas na tela
 window.alert("Olá " + nome + "! Seja bem-vindo(a)!");
